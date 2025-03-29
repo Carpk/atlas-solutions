79fbd72fcb1807bb12da2afe75c98c70ceeb8905
Invalid <zappeln@gmail.com>
2025-03-28 21:15:06 -0500

    parsing returned dmarc string

--- src/components/DmarcCheck.jsx
+++ src/components/DmarcCheck.jsx
@@ -6,20 +6,22 @@ import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
-// import Paper from '@mui/material/Paper';
-// import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 
-// import axios from './axios'
+import axios from './axios'
 
-// const Item = styled(Paper)(({ theme }) => ({
-//   backgroundColor: '#fff',
-//   padding: theme.spacing(1),
-//   textAlign: 'center',
-//   color: theme.palette.text.secondary,
-// }));
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function DmarcCheck() {
-    // const [dmarcCheck, setDmarcCheck] = useState([])
+    const [dmarcCheck, setDmarcCheck] = useState("blank")
    
     // useEffect(() => {
     //   async function fetchData() {
@@ -28,14 +30,38 @@ export default function DmarcCheck() {
     //   }
     //   fetchData()
     // }, [])
+
+    function handleCheck(event) {
+      const domain = "bridgewaterstudio.net"
+      console.log(event)
+      async function fetchData() {
+        const req = await axios.get(`https://dns.google/resolve?name=_dmarc.${domain}&type=TXT`)
+          setDmarcCheck(req.data.Answer[0].data)
+        }
+        fetchData()
+    }
   
     return (
       <Stack spacing={2} sx={{ display: 'flex', mt: 20 }}>
         
-        <TextField id="domain-field" label="domain" variant="outlined" />
-        <Button variant="contained">Check</Button>
+        <TextField 
+          id="domain-field" 
+          label="domain" 
+          variant="outlined" 
+          
+        />
+        <Button variant="contained" onClick={handleCheck}>Check</Button>
 
+        <Item >
+          { dmarcCheck }
+        </Item>
 
+        <Item>
+          { dmarcCheck.split(";").map((item, index) => {
+            return <div key={index}>{item}</div>;
+          })}
+
+        </Item>
 
         
       </Stack>
